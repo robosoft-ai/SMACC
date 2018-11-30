b8ca78b1824989020ff786c7718844b6ecd1ffc2
Pablo Iñigo Blasco <pibgeus@gmail.com>
2018-11-30 19:22:38 +0100

    refactoring smacc_planner_switcher

--- smacc_navigation/smacc_planner_switcher/src/planner_switcher.cpp
+++ smacc_navigation/smacc_planner_switcher/src/planner_switcher.cpp
@@ -3,4 +3,4 @@
  * 	 Authors: Pablo Inigo Blasco, Brett Aldrich
  *
  ******************************************************************************************************************/
-#include <smacc_odom_tracker/planner_switcher.h>
+#include <smacc_planner_switcher/planner_switcher.h>
