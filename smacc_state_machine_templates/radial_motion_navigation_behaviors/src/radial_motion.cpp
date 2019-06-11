dc1d00beeab412da783bbcd8a4988bbbdebc4de1
Pablo Iñigo Blasco <pibgeus@gmail.com>
2019-06-11 17:24:09 +0200

    minor header refactoring

--- smacc_state_machine_templates/radial_motion_navigation_behaviors/src/radial_motion.cpp
+++ smacc_state_machine_templates/radial_motion_navigation_behaviors/src/radial_motion.cpp
@@ -1,10 +1,4 @@
 #include <radial_motion.h>
-#include <boost/thread.hpp>
-
-#include <states/navigate_to_radial_start.h>
-#include <states/navigate_to_end_point.h>
-#include <states/return_to_radial_start.h>
-#include <states/rotate_degrees.h>
 
 // state behaviors
 #include <substates_behaviors/tool/tool_start.h>
