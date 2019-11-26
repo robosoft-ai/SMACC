01cb6fe4a2ec3690944ff7efd6de21c9c7962be9
Pablo Iñigo Blasco <pibgeus@gmail.com>
2019-11-26 21:35:02 +0100

    removing sm_radial_motion example. It is redundant with sm_dance_bot

--- smacc_sm_examples/sm_radial_motion/include/sm_radial_motion/states.h
+++
@@ -1,6 +0,0 @@
-#include <sm_radial_motion/state_names.h>
-#include <sm_radial_motion/states/navigate_superstate.h>
-#include <sm_radial_motion/states/navigate_to_radial_start.h>
-#include <sm_radial_motion/states/navigate_to_end_point.h>
-#include <sm_radial_motion/states/return_to_radial_start.h>
-#include <sm_radial_motion/states/rotate_degrees.h>
--- smacc_sm_examples/sm_radial_motion/include/sm_radial_motion/substate_behaviors.h
+++
@@ -1,6 +0,0 @@
-#include <sm_radial_motion/substates_behaviors/navigation/navigate_forward.h>
-#include <sm_radial_motion/substates_behaviors/navigation/navigate_global_position.h>
-#include <sm_radial_motion/substates_behaviors/navigation/undo_path_backwards.h>
-#include <sm_radial_motion/substates_behaviors/navigation/rotate.h>
-#include <sm_radial_motion/substates_behaviors/tool/tool_start.h>
-#include <sm_radial_motion/substates_behaviors/tool/tool_stop.h>
