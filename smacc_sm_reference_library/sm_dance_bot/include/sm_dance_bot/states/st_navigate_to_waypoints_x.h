8a1410ff6d7d84a28ca63fb5f91d8b07bc731be3
Pablo Iñigo Blasco <pibgeus@gmail.com>
2019-12-23 20:39:17 +0100

    navigateWaypointsX refactoring

--- smacc_sm_reference_library/sm_dance_bot/include/sm_dance_bot/states/st_navigate_to_waypoints_x.h
+++ smacc_sm_reference_library/sm_dance_bot/include/sm_dance_bot/states/st_navigate_to_waypoints_x.h
@@ -24,7 +24,7 @@ struct StNavigateToWaypointsX : smacc::SmaccState<StNavigateToWaypointsX, MsDanc
   };
 
   typedef mpl::list<
-      
+
       smacc::transition<EvWaypoint0<ClMoveBaseZ, OrNavigation>, sm_dance_bot::SS1::SsRadialPattern1, TRANSITION_1>,
       smacc::transition<EvWaypoint1<ClMoveBaseZ, OrNavigation>, sm_dance_bot::SS2::SsRadialPattern2, TRANSITION_2>,
       smacc::transition<EvWaypoint2<ClMoveBaseZ, OrNavigation>, sm_dance_bot::SS3::SsRadialPattern3, TRANSITION_3>,
@@ -42,83 +42,23 @@ struct StNavigateToWaypointsX : smacc::SmaccState<StNavigateToWaypointsX, MsDanc
   {
     static_configure<OrTool, CbToolStart>();
     static_configure<OrObstaclePerception, CbLidarSensor>();
-
-    // example for temperature sensor
-
-    //std::function<bool(EvTopicMessage<CbConditionClTemperatureSensor> *)> temperatureWarningCondition = ([](EvTopicMessage<CbConditionClTemperatureSensor> *ev) {
-    //  return ev->msgData.temperature > 30;
-    //});
-    //static_createLogicUnit<LuConditional, EvTrue<LuConditional>, mpl::list<EvTopicMessage<CbConditionClTemperatureSensor>>>(temperatureWarningCondition);
-
-    //smacc::transition<EvTrue<LuConditional>, StNavigateToWaypointsX>>
-
-    /*
-    std::vector<Pose2D> waypoints =
-        {
-            {3.0, -2.0, 0},
-            {-5.5, 3.0, 0},
-            {-11.0, -5.0, 0},
-            {2.0, -8.58, 0},
-            {-10.0, 14.5, 0}};
-*/
-    //static_configure<OrNavigation, CbWaypointsNavigator>(waypoints);
-
-    /*
-    static_createLogicUnit<LuRoundRobin, 
-                              mpl::list<
-                                   EvWaypoint1<StNavigateToWaypointsX>,
-                                   EvWaypoint2<StNavigateToWaypointsX>,
-                                   EvWaypoint3<StNavigateToWaypointsX>, 
-                                   EvWaypoint4<StNavigateToWaypointsX>,
-                                   EvWaypoint5<StNavigateToWaypointsX>
-                                  >, 
-                         mpl::list<EvActionSucceeded<smacc::ClMoveBaseZ, OrNavigation>>();
-                         */
-
-    // what about persistence
-    // would we have any output event?
-
-    // WaypointNaigator
-    // it is a logic unit
-    // it does not contain the transition table internally
-
-    // static_transition_dynamic_logic_unit< sm_dance_bot::SS1::SsRadialPattern1,
-    //                                       sm_dance_bot::SS2::SsRadialPattern2,
-    //                                       sm_dance_bot::SS2::SsRadialPattern3>();
-
-    // static_createLogicUnit<lusource, outevent, invent, dststate1, dststate2 ...>();
-    // static_createLogicUnit<LuCounter, EvCountFinish<LuCounter>,    >();
-    // function<T1, T2, Targs...>
-    // function<T1, T2, TOutEvents..., TInEvents...>
-
-    // function<LogicUnitType, mpl::list<>, mpl::list<>>();
-    // function<LogicUnitType, TEvOut, mpl::list<>>();
-    // function<LogicUnitType, mpl::list<>, TEvIn>();
-
-    // static_transition(
-    // (
-    //   {
-    //     {0, typeid(SS1::SsRadialPattern1)},
-    //     {1, typeid(SS1::SsRadialPattern2)}
-    //     {3, typeid(SS1::SsRadialPattern3)}
-    //   }
-    // ));
   }
 
   void onInitialize()
   {
-    // this->currentIteration = 0;
-
-    // // IDEA: this->declarePersistentVariable(&currentIteration)
-    // this->getGlobalSMData("navigation_x_iteration", currentIteration);
-
-    //ROS_INFO("current iteration waypoints x: %d", currentIteration);
-
-    ClMoveBaseZ* move_base;
+    ClMoveBaseZ *move_base;
     this->requiresClient(move_base);
 
     auto waypointsNavigator = move_base->getComponent<WaypointNavigator>();
 
+    loadWaypointsFirstTime(waypointsNavigator);
+
+    waypointsNavigator->sendNextGoal();
+    ROS_INFO("current iteration waypoints x: %ld", waypointsNavigator->getCurrentWaypointIndex());
+  }
+
+  void loadWaypointsFirstTime(WaypointNavigator *navigator)
+  {
     // if it is the first time and the waypoints navigator is not configured
     if (waypointsNavigator->getWaypoints().size() == 0)
     {
@@ -129,10 +69,6 @@ struct StNavigateToWaypointsX : smacc::SmaccState<StNavigateToWaypointsX, MsDanc
         waypointsNavigator->loadWayPointsFromFile(planfilepath);
       }
     }
-
-    //move_base->getComponent<WaypointNavigator>().sendNextGoal();
-    waypointsNavigator->sendNextGoal();
-    ROS_INFO("current iteration waypoints x: %ld", waypointsNavigator->getCurrentWaypointIndex());
   }
 };
 
