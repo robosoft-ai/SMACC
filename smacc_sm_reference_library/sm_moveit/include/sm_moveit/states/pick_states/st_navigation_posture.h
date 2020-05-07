15d94a4bf251cc9872e4662192e6ac6bbc846e29
pablo.inigo.blasco <pablo@ibrobotics.com>
2020-05-07 22:16:08 +0200

    improing sm_moveit demo

--- smacc_sm_reference_library/sm_moveit/include/sm_moveit/states/pick_states/st_navigation_posture.h
+++ smacc_sm_reference_library/sm_moveit/include/sm_moveit/states/pick_states/st_navigation_posture.h
@@ -18,19 +18,22 @@ struct StNavigationPosture : smacc::SmaccState<StNavigationPosture, SS>
     // STATE FUNCTIONS
     static void staticConfigure()
     {
-        std::map<std::string, double> jointValues{
+       std::map<std::string, double> jointValues{
             {"l_wheel_joint", 0.2032989263112741},
             {"r_wheel_joint", -0.25798452342087597},
             {"torso_lift_joint", -1.2577603815717127e-05},
             {"bellows_joint", 0.006785193726937263},
             {"head_pan_joint", 0.0053190193943049024},
             {"head_tilt_joint", 0.0037173708013753526},
-            {"shoulder_pan_joint", 1.3199986235290062},
-            {"shoulder_lift_joint", 0.7000699976533715},
+            //{"shoulder_pan_joint", 0.7199986235290062},
+            {"shoulder_pan_joint", 1.0},
+            //{"shoulder_lift_joint", 0.7000699976533715},
+            {"shoulder_lift_joint", 1.51},
             {"upperarm_roll_joint", -7.398079828480064e-05},
-            {"elbow_flex_joint", -1.99994794099468},
+            //{"elbow_flex_joint", -1.99994794099468},
+            {"elbow_flex_joint", -2.49994794099468},
             {"forearm_roll_joint", 0.00021030168417901507},
-            {"wrist_flex_joint", -0.5700976747076689},
+            {"wrist_flex_joint", -0.89},
             {"wrist_roll_joint", -0.00023467601597193521},
             {"l_gripper_finger_joint", 0.05001998415439018},
             {"r_gripper_finger_joint", 0.050039698895249535}};
