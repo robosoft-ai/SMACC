ca4f48ebbb4221403771c94da50b9be73fcdb37e
Brett Aldrich <brett2@reelrobotics.com>
2020-05-18 18:49:50 -0700

    Final CbTimer Push

---
+++ smacc_sm_reference_library/sm_atomic_cb/include/sm_atomic_cb/orthogonals/or_timer.h
@@ -0,0 +1,15 @@
+#include <smacc/smacc.h>
+#include <ros_timer_client/cl_ros_timer.h>
+
+namespace sm_atomic_cb
+{
+class OrTimer : public smacc::Orthogonal<OrTimer>
+{
+public:
+    virtual void onInitialize() override
+    {
+        auto client = this->createClient<cl_ros_timer::ClRosTimer>(ros::Duration(1));
+        client->initialize();
+    }
+};
+} // namespace sm_atomic_cb
