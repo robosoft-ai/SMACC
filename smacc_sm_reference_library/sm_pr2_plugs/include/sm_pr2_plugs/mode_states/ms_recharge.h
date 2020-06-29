a526fa6c8048eb9cc6a38d97b0d6a229ebc86232
Pablo Iñigo Blasco <pibgeus@gmail.com>
2020-06-29 20:31:39 +0200

    removing pr2

--- smacc_sm_reference_library/sm_pr2_plugs/include/sm_pr2_plugs/mode_states/ms_recharge.h
+++
@@ -1,9 +0,0 @@
-#include <smacc/smacc.h>
-namespace sm_pr2_plugs{
-// STATE DECLARATION
-class MsRecharge : public smacc::SmaccState<MsRecharge, SmPR2Plugs, StProcessRechargeCommand>
-{
-public:
-   using SmaccState::SmaccState;
-};
-} // namespace sm_pr2_plugs
