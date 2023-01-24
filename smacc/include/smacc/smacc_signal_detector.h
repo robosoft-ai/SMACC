4331940880b50d8a0892cea7b33fe3b6ed810acf
Pablo Iñigo Blasco <pablo@ibrobotics.com>
2023-01-24 20:40:27 +0100

    async spinner signal detector. Thread model. (#78)

--- smacc/include/smacc/smacc_signal_detector.h
+++ smacc/include/smacc/smacc_signal_detector.h
@@ -11,64 +11,72 @@
 
 namespace smacc
 {
+enum class ExecutionModel
+{
+  SINGLE_THREAD_SPINNER,
+  ASYNCHRONOUS_SPINNER
+};
+
 class SignalDetector
 {
 public:
-    SignalDetector(SmaccFifoScheduler *scheduler);
+  SignalDetector(SmaccFifoScheduler* scheduler, ExecutionModel executionModel = ExecutionModel::SINGLE_THREAD_SPINNER);
 
-    void initialize(ISmaccStateMachine *stateMachine);
+  void initialize(ISmaccStateMachine* stateMachine);
 
-    void setProcessorHandle(SmaccFifoScheduler::processor_handle processorHandle);
+  void setProcessorHandle(SmaccFifoScheduler::processor_handle processorHandle);
 
-    // Runs the polling loop into a thread...
-    void runThread();
+  // Runs the polling loop into a thread...
+  void runThread();
 
-    // Waits for the polling thread to end...
-    void join();
+  // Waits for the polling thread to end...
+  void join();
 
-    void stop();
+  void stop();
 
-    void pollingLoop();
+  void pollingLoop();
 
-    void pollOnce();
+  void pollOnce();
 
-    template <typename EventType>
-    void postEvent(EventType *ev)
-    {
-        boost::intrusive_ptr<EventType> weakPtrEvent = ev;
-        this->scheduler_->queue_event(processorHandle_, weakPtrEvent);
-    }
+  template <typename EventType>
+  void postEvent(EventType* ev)
+  {
+    boost::intrusive_ptr<EventType> weakPtrEvent = ev;
+    this->scheduler_->queue_event(processorHandle_, weakPtrEvent);
+  }
 
 private:
-    ISmaccStateMachine *smaccStateMachine_;
+  ISmaccStateMachine* smaccStateMachine_;
 
-    std::vector<ISmaccUpdatable *> updatableClients_;
+  std::vector<ISmaccUpdatable*> updatableClients_;
 
-    std::vector<ISmaccUpdatable *> updatableStateElements_;
+  std::vector<ISmaccUpdatable*> updatableStateElements_;
 
-    std::atomic<uint64_t> lastState_;
+  std::atomic<uint64_t> lastState_;
 
-    void findUpdatableClients();
-    void findUpdatableStateElements(ISmaccState* currentState);
+  void findUpdatableClients();
+  void findUpdatableStateElements(ISmaccState* currentState);
 
-    // Loop frequency of the signal detector (to check answers from actionservers)
-    double loop_rate_hz;
+  // Loop frequency of the signal detector (to check answers from actionservers)
+  double loop_rate_hz;
 
-    std::atomic<bool> end_;
+  std::atomic<bool> end_;
 
-    std::atomic<bool> initialized_;
+  std::atomic<bool> initialized_;
 
-    ros::NodeHandle nh_;
+  ros::NodeHandle nh_;
 
-    ros::Publisher statusPub_;
+  ros::Publisher statusPub_;
 
-    // ---- boost statechart related ----
+  // ---- boost statechart related ----
 
-    SmaccFifoScheduler *scheduler_;
+  SmaccFifoScheduler* scheduler_;
 
-    SmaccFifoScheduler::processor_handle processorHandle_;
+  SmaccFifoScheduler::processor_handle processorHandle_;
 
-    boost::thread signalDetectorThread_;
+  boost::thread signalDetectorThread_;
+
+  ExecutionModel executionModel_;
 };
 
 // Main entry point for any SMACC state machine
@@ -77,30 +85,29 @@ private:
 // The created thread is for the state machine process
 // it locks the current thread to handle events of the state machine
 template <typename StateMachineType>
-void run()
+void run(ExecutionModel executionModel = ExecutionModel::SINGLE_THREAD_SPINNER)
 {
-    // create the asynchronous state machine scheduler
-    SmaccFifoScheduler scheduler1(true);
+  // create the asynchronous state machine scheduler
+  SmaccFifoScheduler scheduler1(true);
 
-    // create the signalDetector component
-    SignalDetector signalDetector(&scheduler1);
+  // create the signalDetector component
+  SignalDetector signalDetector(&scheduler1, executionModel);
 
-    // create the asynchronous state machine processor
-    SmaccFifoScheduler::processor_handle sm =
-        scheduler1.create_processor<StateMachineType>(&signalDetector);
+  // create the asynchronous state machine processor
+  SmaccFifoScheduler::processor_handle sm = scheduler1.create_processor<StateMachineType>(&signalDetector);
 
-    // initialize the asynchronous state machine processor
-    signalDetector.setProcessorHandle(sm);
+  // initialize the asynchronous state machine processor
+  signalDetector.setProcessorHandle(sm);
 
-    scheduler1.initiate_processor(sm);
+  scheduler1.initiate_processor(sm);
 
-    //create a thread for the asynchronous state machine processor execution
-    boost::thread otherThread(boost::bind(&sc::fifo_scheduler<>::operator(), &scheduler1, 0));
+  // create a thread for the asynchronous state machine processor execution
+  boost::thread otherThread(boost::bind(&sc::fifo_scheduler<>::operator(), &scheduler1, 0));
 
-    // use the  main thread for the signal detector component (waiting actionclient requests)
-    signalDetector.pollingLoop();
-    scheduler1.terminate();
-    otherThread.join();
+  // use the  main thread for the signal detector component (waiting actionclient requests)
+  signalDetector.pollingLoop();
+  scheduler1.terminate();
+  otherThread.join();
 }
 
-} // namespace smacc
+}  // namespace smacc
